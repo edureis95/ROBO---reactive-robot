927a1571ee1cea2fcc1be37aa129c35b15c52e56
edureis95 <up201303832@gcloud.fe.up.pt>
2017-11-08 00:25:42 +0000

    Script de execução feito

---
+++ runReactiveRobot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/catkin_ws
+. devel/setup.bash
+gnome-terminal -x sh -c "roslaunch stdr_launchers server_no_map.launch"
+gnome-terminal -x sh -c "roslaunch stdr_gui stdr_gui.launch"
+catkin_make --pkg reactive_robot
+roscd stdr_resources
+rosrun stdr_server load_map maps/D.yaml
+rosrun stdr_robot robot_handler add resources/robots/pandora_robot.yaml 9 1.5 1.57
+rosrun reactive_robot reactive_robot_node
