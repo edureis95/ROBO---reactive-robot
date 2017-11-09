2afeee70cd367837e26d99e847fa3f988f5c62f4
edureis95 <up201303832@gcloud.fe.up.pt>
2017-11-09 12:18:43 +0000

    Alteração no script

--- runReactiveRobot.sh
+++ runReactiveRobot.sh
@@ -7,5 +7,8 @@ gnome-terminal -x sh -c "roslaunch stdr_gui stdr_gui.launch"
 catkin_make --pkg reactive_robot
 roscd stdr_resources
 rosrun stdr_server load_map maps/D.yaml
-rosrun stdr_robot robot_handler add resources/robots/pandora_robot.yaml 9 1.5 1.57
+#D grande
+#rosrun stdr_robot robot_handler add resources/robots/pandora_robot.yaml 9 11 1.57
+#D pequeno
+rosrun stdr_robot robot_handler add resources/robots/pandora_robot.yaml 9 5 1.57
 rosrun reactive_robot reactive_robot_node
